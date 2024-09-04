5364b29c9a16637a46ecdad4960a5e84f9c43f0a
Christian7547 <gec0028228@est.univalle.edu>
2024-09-03 23:39:06 -0400

    Init commit
    Initial commit

---
+++ src/main/java/com/PRIII_24_MOSQUITOLAB_API/Priii24MosquitolabApiApplication.java
@@ -0,0 +1,13 @@
+package com.PRIII_24_MOSQUITOLAB_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Priii24MosquitolabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Priii24MosquitolabApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PRIII_24_MOSQUITOLAB_API/Priii24MosquitolabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PRIII_24_MOSQUITOLAB_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Priii24MosquitolabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
