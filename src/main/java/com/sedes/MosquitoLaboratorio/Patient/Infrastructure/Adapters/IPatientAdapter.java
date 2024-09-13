fd5268721fbfd0a3d447d288be0c5a8dbe1e94c4
Chris0028 <gec0028228@est.univalle.edu>
2024-09-12 21:25:20 -0400

    Reset repository

--- src/main/java/com/sedes/MosquitoLaboratorio/Patient/Infrastructure/Adapters/IPatientAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.sedes.MosquitoLaboratorio.Patient.Infrastructure.Adapters;
-
-import com.sedes.MosquitoLaboratorio.Patient.Infrastructure.Entities.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPatientAdapter extends JpaRepository<Patient, Long> {
-}
