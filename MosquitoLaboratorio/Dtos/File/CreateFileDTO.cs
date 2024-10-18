44115a2d65cca8ff921a0d6c1512ac3275b41228
YCerezaY <143458829+YCerezaY@users.noreply.github.com>
2024-10-18 00:19:54 -0400

    Fixed Dtos

--- MosquitoLaboratorio/Dtos/File/CreateFileDTO.cs
+++ MosquitoLaboratorio/Dtos/File/CreateFileDTO.cs
@@ -3,6 +3,9 @@ using System.Text.Json.Serialization;
 
 namespace MosquitoLaboratorio.Dtos.File
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
     public class CreateFileDTO
     {
         // Parametros Patient
@@ -11,16 +14,35 @@ namespace MosquitoLaboratorio.Dtos.File
         public string PatientSecondLastName { get; set; }
         public string PatientGender { get; set; }
         public string PatientCi { get; set; }
+
         [DataType(DataType.Date)]
-        public DateTime PatientBirthDate { get; set; }
+        public DateTime PatientBirthDate
+        {
+            get => _patientBirthDate;
+            set => _patientBirthDate = DateTime.SpecifyKind(value, DateTimeKind.Utc);
+        }
+        private DateTime _patientBirthDate;
+
         public string PatientPhone { get; set; }
         public string PatientCode { get; set; }
 
         // Parametros Pregnant
         [DataType(DataType.Date)]
-        public DateTime? PregnantLastMenstruationDate { get; set; }
+        public DateTime? PregnantLastMenstruationDate
+        {
+            get => _pregnantLastMenstruationDate;
+            set => _pregnantLastMenstruationDate = value.HasValue ? DateTime.SpecifyKind(value.Value, DateTimeKind.Utc) : (DateTime?)null;
+        }
+        private DateTime? _pregnantLastMenstruationDate;
+
         [DataType(DataType.Date)]
-        public DateTime? PregnantChildBirthDate { get; set; }
+        public DateTime? PregnantChildBirthDate
+        {
+            get => _pregnantChildBirthDate;
+            set => _pregnantChildBirthDate = value.HasValue ? DateTime.SpecifyKind(value.Value, DateTimeKind.Utc) : (DateTime?)null;
+        }
+        private DateTime? _pregnantChildBirthDate;
+
         public string? PregnantDisease { get; set; }
 
         // Parametros Child (Nullable)
@@ -47,25 +69,50 @@ namespace MosquitoLaboratorio.Dtos.File
 
         // Parametros Hospitalized 
         [DataType(DataType.Date)]
-        public DateTime? HospitalizedEntryDate { get; set; }
+        public DateTime? HospitalizedEntryDate
+        {
+            get => _hospitalizedEntryDate;
+            set => _hospitalizedEntryDate = value.HasValue ? DateTime.SpecifyKind(value.Value, DateTimeKind.Utc) : (DateTime?)null;
+        }
+        private DateTime? _hospitalizedEntryDate;
+
         public short? HospitalizedType { get; set; }
         public string? HospitalizedName { get; set; }
 
         // Parametros Hospitalized UTI
         [DataType(DataType.Date)]
-        public DateTime? UtiEntryDate { get; set; }
+        public DateTime? UtiEntryDate
+        {
+            get => _utiEntryDate;
+            set => _utiEntryDate = value.HasValue ? DateTime.SpecifyKind(value.Value, DateTimeKind.Utc) : (DateTime?)null;
+        }
+        private DateTime? _utiEntryDate;
+
         public short? UtiType { get; set; }
         public string? UtiName { get; set; }
 
         // Parametros Dischargehospitalized
         public string? DischargeType { get; set; }
+
         [DataType(DataType.Date)]
-        public DateTime? DischargeDate { get; set; }
+        public DateTime? DischargeDate
+        {
+            get => _dischargeDate;
+            set => _dischargeDate = value.HasValue ? DateTime.SpecifyKind(value.Value, DateTimeKind.Utc) : (DateTime?)null;
+        }
+        private DateTime? _dischargeDate;
 
         // Parametros File
         public string FileCode { get; set; }
+
         [DataType(DataType.Date)]
-        public DateTime FileSymptomsDate { get; set; }
+        public DateTime FileSymptomsDate
+        {
+            get => _fileSymptomsDate;
+            set => _fileSymptomsDate = DateTime.SpecifyKind(value, DateTimeKind.Utc);
+        }
+        private DateTime _fileSymptomsDate;
+
         public string FileDiscoveryMethod { get; set; }
         public string FileEpidemiologicalWeek { get; set; }
 
@@ -76,8 +123,15 @@ namespace MosquitoLaboratorio.Dtos.File
 
         // Parametros Sample
         public string? SampleSampleType { get; set; }
+
         [DataType(DataType.Date)]
-        public DateTime? SampleCollectionDate { get; set; }
+        public DateTime? SampleCollectionDate
+        {
+            get => _sampleCollectionDate;
+            set => _sampleCollectionDate = value.HasValue ? DateTime.SpecifyKind(value.Value, DateTimeKind.Utc) : (DateTime?)null;
+        }
+        private DateTime? _sampleCollectionDate;
+
         public string? SampleObservation { get; set; }
 
         // Parametros Test
@@ -91,5 +145,6 @@ namespace MosquitoLaboratorio.Dtos.File
         public int[] Symptoms { get; set; }
         public string[] IsSymptomsPresent { get; set; }
     }
+
 
 }
